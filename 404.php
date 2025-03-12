c80dc00c5a2d4b383a1ec76ed0058d08fa527e95
Shalim Mora <72850652+Shalimmo@users.noreply.github.com>
2025-03-12 15:10:37 -0600

    Update 404.php
    php update

--- 404.php
+++ 404.php
@@ -1,65 +1,65 @@
-<?php get_header();
+<?php get_header(); ?>
 
+<?php
 $default_header = get_field("default_header", 'option');
 $thumb_thumb_url = $default_header['sizes']['jmvillas-medium'];
 $thumb_medium_url = $default_header['sizes']['jmvillas-full'];
-
 ?>
 
 <header class="content--header">
-	<figure>
-		<picture>
-			<source srcset="<?php echo $thumb_thumb_url; ?>" media="(max-width: 400px)">
-			<img srcset="<?php echo $thumb_medium_url; ?>" alt="<?php the_title(); ?>">
-		</picture>
-	</figure>
+    <figure>
+        <picture>
+            <source srcset="<?php echo $thumb_thumb_url; ?>" media="(max-width: 400px)">
+            <img srcset="<?php echo $thumb_medium_url; ?>" alt="<?php the_title(); ?>">
+        </picture>
+    </figure>
 </header>
 
 <div id="content">
 
-	<div class="content--breadcrumb wrap">
-		<?php the_breadcrumb(); ?>
-	</div>
+    <div class="content--breadcrumb wrap">
+        <?php the_breadcrumb(); ?>
+    </div>
 
-	<div id="inner-content" class="content--inner wrap"  role="main">
+    <div id="inner-content" class="content--inner wrap"  role="main">
 
-		<section class="content--page">
+        <section class="content--page">
 
-			<article class="page">
+            <article class="page">
 
-				<header class="page--header">
-							
-					<h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
-			
-				</header> <!-- end article header -->
-		
-				<div class="page--content">
-				
-					<p><?php _e("The Villa you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
-		
-				</div> <!-- end article section -->
+                <header class="page--header">
+                            
+                    <h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
+            
+                </header> <!-- end article header -->
+        
+                <div class="page--content">
+                
+                    <p><?php _e("The Villa you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
+        
+                </div> <!-- end article section -->
 
-				<div class="page--search">
-	
-					<p><?php get_search_form(); ?></p>
-	
-				</div> <!-- end search section -->
-			
-				<footer class="page--footer">
-				
-					<p></p>
-				
-				</footer> <!-- end article footer -->
+                <div class="page--search">
+    
+                    <p><?php get_search_form(); ?></p>
+        
+                </div> <!-- end search section -->
+            
+                <footer class="page--footer">
+                
+                    <p></p>
+                
+                </footer> <!-- end article footer -->
 
-			</article>
+            </article>
 
-		</section> <!-- end .content--page -->
+        </section> <!-- end .content--page -->
 
-		<aside class="sidebar sidebar__home">
-			<?php get_sidebar(); ?>
-		</aside> <!-- end #inner-content -->
-			  
-	</div> <!-- end #inner-content -->
+        <aside class="sidebar sidebar__home">
+            <?php get_sidebar(); ?>
+        </aside> <!-- end #inner-content -->
+              
+    </div> <!-- end #inner-content -->
 </div> <!-- end #content -->
 
 <?php get_footer(); ?>

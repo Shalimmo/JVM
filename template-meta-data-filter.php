a7f642b56d4ea0b87a23ed6e93fda9af95eb6360
Shalim Mora <72850652+Shalimmo@users.noreply.github.com>
2025-03-20 21:06:45 -0600

    Update template-meta-data-filter.php
    Actualización 20 marzo

--- template-meta-data-filter.php
+++ template-meta-data-filter.php
@@ -1,11 +1,16 @@
-<?php if(!defined('ABSPATH')) die('No direct access allowed'); ?>
+<?php 
+if (!defined('ABSPATH')) {
+    exit('No direct access allowed');
+}
+?>
 <?php
 /*
   Template Name: Template MetaData Filter
  */
-if(class_exists('MetaDataFilterPage')) {
+
+if (class_exists('MetaDataFilterPage')) {
     wp_enqueue_style('meta_data_filter_front', MetaDataFilterCore::get_application_uri() . 'css/front.css');
-    wp_enqueue_script('meta_data_filter_widget', MetaDataFilterCore::get_application_uri() . 'js/front.js', array('jquery'), null, true);
+    wp_enqueue_script('meta_data_filter_widget', MetaDataFilterCore::get_application_uri() . 'js/front.js', ['jquery'], null, true);
 }
 
 get_header();
@@ -13,7 +18,7 @@ get_header();
 
 <!-- - - - - - - - - - - - Entry - - - - - - - - - - - - - - -->
 <div id="mdf_output">
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <?php
             the_content();
             wp_reset_postdata(); // Use wp_reset_postdata instead of wp_reset_query
